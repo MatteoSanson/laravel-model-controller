41fdb6e030e454d90484b92586de734348aefaf5
MatteoSanson <matteo.sanson@gmail.com>
2024-01-29 21:28:08 +0100

    feat: card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,8 @@
                         <div class="card h-100 w-100">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $movie['title'] }}</h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie['original_title'] }}</h6>
+                                <h6 class="card-subtitle pt-2 pb-4 text-body-secondary">{{ $movie['original_title'] }}
+                                </h6>
                                 <ul class="list-unstyled">
                                     <li>
                                         <p>{{ $movie['nationality'] }}</p>
